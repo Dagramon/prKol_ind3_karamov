1be9c599ee26cdb7bbb890e6759c54d1f841e0ba
Neven <DenisGorbatuk77@gmail.com>
2025-04-02 10:03:48 +0500

    Добавьте файлы проекта.

---
+++ Class1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prKol_ind3_karamov
+{
+    class Complex
+    {
+        private double a;
+        private double b;
+        public double A
+        {
+            get { return a; }
+            set { a = value; }
+        }
+            public double B
+        {
+            get { return b; }
+            set { b = value; }
+        }
+        public static string sum(double a1, double b1, double a2, double b2)
+        {
+            double sum1 = a1 + a2;
+            double sum2 = b1 + b2;
+            return $"{sum1} + {sum2}i";
+        }
+        public static string razn(double a1, double b1, double a2, double b2)
+        {
+            double sum1 = a1 - a2;
+            double sum2 = b1 - b2;
+            return $"{sum1} + {sum2}i";
+        }
+        public static string proizv(double a1, double b1, double a2, double b2)
+        {
+            double sum1 = a1*a2 - b1*b2;
+            double sum2 = a1*b2 + b1*a2;
+            return $"{sum1} + {sum2}i";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace prKol_ind3_karamov
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Complex number1 = new Complex();
+                Complex number2 = new Complex();
+                ArrayList results = new ArrayList();
+                Console.Write("Введите a в первом комплексном числе: ");
+                double a = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Введите b в первом комплексном числе: ");
+                double b = Convert.ToInt32(Console.ReadLine());
+                number1.A = a;
+                number1.B = b;
+                Console.WriteLine($"Первое число: {number1.A} + {number1.B}i");
+                Console.Write("Введите a во втором комплексном числе: ");
+                a = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Введите b во втором комплексном числе: ");
+                b = Convert.ToInt32(Console.ReadLine());
+                number2.A = a;
+                number2.B = b;
+                Console.WriteLine($"Второе число: {number2.A} + {number2.B}i");
+                Console.WriteLine("Результаты:");
+                results.Add("Сумма: ");
+                results.Add(Complex.sum(number1.A, number1.B, number2.A, number2.B));
+                results.Add("Разность: ");
+                results.Add(Complex.razn(number1.A, number1.B, number2.A, number2.B));
+                results.Add("Произведение: ");
+                results.Add(Complex.proizv(number1.A, number1.B, number2.A, number2.B));
+                foreach (object item in results)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Неправильный формат ввода");
+            }
+            Console.ReadLine();
+        }
+    }
+}
